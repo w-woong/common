c673d4cab8e93fe828d18dbf0a33b4dfaf1d20a7
“wonkdance” <wonkdance@gmail.com>
2022-10-14 23:13:18 +0900

    fix http

--- http.go
+++ http.go
@@ -15,5 +15,22 @@ func HttpErrorWithMessage(w http.ResponseWriter, message string, status int) {
 	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.WriteHeader(status)
 
-	si.EncodeJson(w, dto.NewHttpBody(message, status))
+	si.EncodeJson(w, NewHttpBody(message, status))
+}
+
+type HttpBody struct {
+	Status    int         `json:"status,omitempty"`
+	Message   string      `json:"message,omitempty"`
+	Count     int         `json:"count,omitempty"`
+	Document  interface{} `json:"document,omitempty"`
+	Documents interface{} `json:"documents,omitempty"`
+}
+
+var HttpBodyOK = HttpBody{Status: http.StatusOK}
+
+func NewHttpBody(message string, status int) *HttpBody {
+	return &HttpBody{
+		Status:  status,
+		Message: message,
+	}
 }
