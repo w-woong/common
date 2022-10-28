b90a03ab53107f5e438a9870a1045c29f4f44bce
“wonkdance” <wonkdance@gmail.com>
2022-10-28 22:21:53 +0900

    add: tx

---
+++ tx.go
@@ -0,0 +1,12 @@
+package common
+
+//go:generate mockgen -destination=./mocks/mock_tx.go -package=mocks -mock_names=TxBeginner=MockTxBeginner,TxController=MockTxController -source=./tx.go . TxBeginner,TxController
+
+type TxBeginner interface {
+	Begin() (TxController, error)
+}
+
+type TxController interface {
+	Commit() error
+	Rollback() error
+}
