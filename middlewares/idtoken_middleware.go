ca0a66cbf2fd34dfc57a9383928e3a5d1caff277
“wonkdance” <wonkdance@gmail.com>
2022-12-04 23:18:19 +0900

    Add token source key

--- middlewares/idtoken_middleware.go
+++ middlewares/idtoken_middleware.go
@@ -56,6 +56,7 @@ func AuthIDTokenHandler(next http.HandlerFunc, validator validators.IDTokenValid
 				return
 			}
 			ctx = context.WithValue(ctx, validators.IDTokenClaimsKey{}, *claims)
+			ctx = context.WithValue(ctx, validators.TokenSourceKey{}, tokenSource)
 		}
 
 		next.ServeHTTP(w, r.WithContext(ctx))
