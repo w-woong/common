99d1a3aac42384ea42fc022f9aa0ca3621b2c182
“wonkdance” <wonkdance@gmail.com>
2022-12-10 12:51:21 +0900

    Add register user test

---
+++ adapter/tests/user_http_test.go
@@ -0,0 +1,28 @@
+package adapter_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/go-wonk/si/sihttp"
+	"github.com/stretchr/testify/assert"
+	"github.com/w-woong/common/adapter"
+	"github.com/w-woong/common/dto"
+)
+
+func Test_userHttp_RegisterUser(t *testing.T) {
+
+	if !onlinetest {
+		t.Skip("skipping online tests")
+	}
+
+	ctx := context.Background()
+	userHttp := adapter.NewUserHttp(sihttp.DefaultInsecureClient(), "https://localhost:8080", "ab2316584873095f017f6dfa7a9415794f563fcc473eb3fe65b9167e37fd5a4b", "tid", "id_token")
+	_, err := userHttp.RegisterUser(ctx, "google", dto.User{
+		LoginID:     "asdf",
+		LoginType:   "token",
+		LoginSource: "google",
+	})
+
+	assert.Nil(t, err)
+}
