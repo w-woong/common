1537e7ce49d89779b7272cfc2e2314559de144fa
“wonkdance” <wonkdance@gmail.com>
2023-02-06 18:52:51 +0900

    Modify token validator

--- adapter/idtoken_validator.go
+++ adapter/idtoken_validator.go
@@ -66,7 +66,11 @@ func (u *jwksIDTokenValidator) Validate(idToken string) (*jwt.Token, *dto.IDToke
 	} else if errors.Is(err, jwt.ErrTokenMalformed) {
 		return nil, nil, common.ErrTokenMalformed
 	} else if errors.Is(err, jwt.ErrTokenExpired) {
-		return nil, nil, common.ErrTokenExpired
+		claims, ok := token.Claims.(*dto.IDTokenClaims)
+		if !ok {
+			return nil, nil, common.ErrTokenExpired
+		}
+		return token, claims, common.ErrTokenExpired
 	} else if errors.Is(err, jwt.ErrTokenNotValidYet) {
 		// return nil, auth.ErrTokenNotValidYet
 		claims, ok := token.Claims.(*dto.IDTokenClaims)
@@ -82,8 +86,59 @@ func (u *jwksIDTokenValidator) Validate(idToken string) (*jwt.Token, *dto.IDToke
 		return token, claims, nil
 	}
 
-	return nil, nil, err
+	claims, ok := token.Claims.(*dto.IDTokenClaims)
+	if !ok {
+		return nil, nil, err
+	}
+	return token, claims, err
 }
+
+// func (u *jwksIDTokenValidator) Validate(idToken string) (*jwt.Token, *dto.IDTokenClaims, error) {
+// 	if idToken == "" {
+// 		return nil, nil, common.ErrIDTokenNotFound
+// 	}
+
+// 	jwksJson, err := u.jwksStore.Get()
+// 	if err != nil {
+// 		return nil, nil, err
+// 	}
+
+// 	// Create the JWKS from the resource at the given URL.
+// 	jwks, err := keyfunc.NewJSON(jwksJson)
+// 	if err != nil {
+// 		return nil, nil, err
+// 	}
+
+// 	// Parse the JWT.
+// 	// token, err := jwt.Parse(idToken, jwks.Keyfunc)
+// 	token, err := jwt.ParseWithClaims(idToken, &dto.IDTokenClaims{}, jwks.Keyfunc)
+// 	if token.Valid {
+// 		claims, ok := token.Claims.(*dto.IDTokenClaims)
+// 		if !ok {
+// 			return nil, nil, common.ErrUnexpectedTokenClaims
+// 		}
+// 		return token, claims, err
+// 	} else if errors.Is(err, jwt.ErrTokenMalformed) {
+// 		return nil, nil, common.ErrTokenMalformed
+// 	} else if errors.Is(err, jwt.ErrTokenExpired) {
+// 		return nil, nil, common.ErrTokenExpired
+// 	} else if errors.Is(err, jwt.ErrTokenNotValidYet) {
+// 		// return nil, auth.ErrTokenNotValidYet
+// 		claims, ok := token.Claims.(*dto.IDTokenClaims)
+// 		if !ok {
+// 			return nil, nil, common.ErrUnexpectedTokenClaims
+// 		}
+// 		return token, claims, nil
+// 	} else if errors.Is(err, jwt.ErrTokenUsedBeforeIssued) {
+// 		claims, ok := token.Claims.(*dto.IDTokenClaims)
+// 		if !ok {
+// 			return nil, nil, common.ErrUnexpectedTokenClaims
+// 		}
+// 		return token, claims, nil
+// 	}
+
+// 	return nil, nil, err
+// }
 
 // func GetJwks(url string) (json.RawMessage, error) {
 // 	// TODO: cache jwks
