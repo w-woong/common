f67625c00573181163ba5ba3d3bdd7703b4874a3
wksung <spowon@spotv.net>
2023-03-03 11:48:45 +0900

    Fix cookie, add gin middleware

--- adapter/cookie_adapter.go
+++ adapter/cookie_adapter.go
@@ -6,31 +6,31 @@ import (
 )
 
 type SecureCookie struct {
-	expireAfter   time.Duration
-	idTokenCookie string
-	mode          http.SameSite
-	domain        string
-	path          string
+	expireAfter time.Duration
+	name        string
+	mode        http.SameSite
+	domain      string
+	path        string
 }
 
 func NewSecureCookie(expireAfter time.Duration, mode http.SameSite,
-	idTokenCookie string, domain string, path string) *SecureCookie {
+	name string, domain string, path string) *SecureCookie {
 
 	return &SecureCookie{
-		expireAfter:   expireAfter,
-		idTokenCookie: idTokenCookie,
-		mode:          mode,
-		domain:        domain,
-		path:          path,
+		expireAfter: expireAfter,
+		name:        name,
+		mode:        mode,
+		domain:      domain,
+		path:        path,
 	}
 }
 
 func (a *SecureCookie) Get(r *http.Request) string {
-	return get(r, a.idTokenCookie)
+	return get(r, a.name)
 }
 
 func (a *SecureCookie) Set(w http.ResponseWriter, idToken string) {
-	set(w, a.mode, a.idTokenCookie, idToken, a.expireAfter, 0, a.domain, a.path)
+	set(w, a.mode, a.name, idToken, a.expireAfter, 0, a.domain, a.path)
 }
 
 func set(w http.ResponseWriter, sameSiteMode http.SameSite, name, value string, expireAfter time.Duration, maxAge int, domain string, path string) {
